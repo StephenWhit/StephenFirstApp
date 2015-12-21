9c688b4146d460e349259ea24998957c8be104b5
stephen whitcomb <stephen whitcomb>
2015-12-21 16:43:02 -0500

    initial commit

---
+++ StephenTestApp/FirstClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StephenTestApp
+{
+    
+    public class FirstClass
+    {
+        public RelayCommand printCommand { get; set; }
+        public int counter;
+        public int poopProperty
+        {
+            get
+            {
+                return 2 + counter++;
+            }
+            set
+            {
+
+            }
+        }
+        public FirstClass()
+        {
+            printCommand = new RelayCommand(printer);
+            counter = 1;
+        }
+        public void printer(object parameter)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine(poopProperty);
+        }
+    }
+}
